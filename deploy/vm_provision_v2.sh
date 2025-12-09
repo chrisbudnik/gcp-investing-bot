e9ae361280ec62fc11c54a1b25f68481340ebe54
chrisb <chrism.budnik@gmail.com>
2025-12-10 00:39:06 +0100

    update with new debugging info

--- deploy/vm_provision_v2.sh
+++ deploy/vm_provision_v2.sh
@@ -13,6 +13,8 @@ echo "Starting VM Provisioning..."
 
 sudo apt-get update
 sudo apt-get upgrade
+
+echo "[Python] Installing build dependencies."
 sudo apt-get install -y \
     make \
     build-essential \
@@ -31,15 +33,24 @@ sudo apt-get install -y \
     liblzma-dev \
     tk-dev
 
+# 2. Install Python 3.14.2 from source
+echo "[Python] Downloading and installing Python 3.14.2 from source."
+
 cd /tmp/
 wget https://www.python.org/ftp/python/3.14.2/Python-3.14.2.tgz
 tar xzf Python-3.14.2.tgz
 cd Python-3.14.2
 
+# 3. Build and install
+echo "[Python] Building and installing Python 3.14.2."
+
 sudo ./configure --prefix=/opt/python/3.14.2/ --enable-optimizations --with-lto --with-computed-gotos --with-system-ffi --enable-shared --enable-loadable-sqlite-extensions --with-openssl=/usr/local/ssl
 sudo make -j "$(grep -c ^processor /proc/cpuinfo)"
 sudo make altinstall
 sudo rm /tmp/Python-3.14.2.tgz
+
+# 4. Create symlinks
+echo "[Python] Creating symlinks for Python 3.14.2 binaries."
 
 sudo ln -s /opt/python/3.14.2/bin/python3.14        /opt/python/3.14.2/bin/python3
 sudo ln -s /opt/python/3.14.2/bin/python3.14        /opt/python/3.14.2/bin/python
@@ -48,20 +59,20 @@ sudo ln -s /opt/python/3.14.2/bin/pip3.14           /opt/python/3.14.2/bin/pip
 sudo ln -s /opt/python/3.14.2/bin/pydoc3.14         /opt/python/3.14.2/bin/pydoc
 sudo ln -s /opt/python/3.14.2/bin/idle3.14          /opt/python/3.14.2/bin/idle
 sudo ln -s /opt/python/3.14.2/bin/python3.14-config   /opt/python/3.14.2/bin/python-config
-sudo /opt/python/3.14.2/bin/python3.14 -m pip install --upgrade pip setuptools wheel
 
+# 5. Update pip and install uv
+echo "[Python] Upgrading pip and installing uv."
+
+sudo /opt/python/3.14.2/bin/python3.14 -m pip install --upgrade pip setuptools wheel
 sudo /opt/python/3.14.2/bin/python3.14 -m pip install uv
 
 
-pip3 install --upgrade pip
-pip3 install uv
+# 6. Check installation
+# echo "Verifying installations..."
 
-
-# 3. Check installation
-echo "Verifying installations..."
-git --version
-python3 --version
-pip3 --version
-uv --version
+# git --version
+# python3 --version
+# pip3 --version
+# uv --version
 
 echo "VM Provisioning complete."
